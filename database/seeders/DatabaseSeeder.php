9702ca6566cba05e7794c3b2f1f5b01281fcbc90
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-19 17:49:05 +0200

    add calll

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CocktailSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +13,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    {   
+        $this->call(CocktailSeeder::class);
     }
+    
 }
