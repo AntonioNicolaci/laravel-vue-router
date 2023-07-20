c0924b728e0db6e1f9dcf3ca7f6093755dcfed42
antonionicolaci <antonionicolaci48@gmail.com>
2023-07-20 16:20:38 +0200

    add cetegory route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\CocktailController;
 
 Route::get('cocktails', [CocktailController::class, 'index'])->name('api.cocktails.index');
+Route::get('category', [CategoryController::class, 'index'])->name('api.category.index');
